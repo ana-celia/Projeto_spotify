53c91b70593c78c9ae2e98df10324ba4727ee12a
Ana Célia Araújo <112821127+ana-celia@users.noreply.github.com>
2025-10-23 23:31:01 +0000

    primeiro commit criando o main()

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Implementar um programa em Java/C# que utiliza a API do Spotify para buscar músicas de um artista
+//e armazenar os dados obtidos em uma estrutura de dados linear (como uma Lista Ligada)
+//implementada por você. O programa deve ser capaz de inserir os dados de duas maneiras distintas:
+//pela ordem de relevância (padrão da API) ou em ordem alfabética pelo nome da música.
+
+using System;
+using System.Collections;
+
+
+
+class Node
+{
+
+}
+
+class BinaryTree
+{
+    
+}
+class Program
+{
+    public static void Main()
+    {
+
+    }
+
+}
